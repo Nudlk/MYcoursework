e2829b6818c79373d2a667dc137d018779e6152a
Nudlk <159611741+Nudlk@users.noreply.github.com>
2024-05-23 17:12:29 +0000

    footer remake

--- guild-cheresnya/src/components/footer/footer.js
+++ guild-cheresnya/src/components/footer/footer.js
@@ -1,9 +1,12 @@
 import './footer-styles.css';
 export default () => {
     return (
-    <footer>
-        <p><h2>Guild Cheresnya</h2></p>
-        <p><h2>Контакты:</h2></p>
-        </footer>)
-    
+    <div className='main-footer'>
+        <div className='container'>
+            <div className='Title'>
+                <img src='images/Title.png'/>
+            </div>
+        </div>
+    </div>
+    )
 }
