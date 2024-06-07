77ab7e58953e3e3b5800edc8584a4e16989bdbd9
Nudlk <159611741+Nudlk@users.noreply.github.com>
2024-06-07 23:07:37 +0000

    review data update

--- guild-cheresnya/src/components/footer/footer.js
+++ guild-cheresnya/src/components/footer/footer.js
@@ -2,10 +2,8 @@ import './footer-styles.css';
 export default () => {
     return (
     <div className='main-footer'>
-        <div className='container'>
-            <div className='Title'>
-                <img src='images/Title.png'/>
-            </div>
+        <div className='Title'>
+            <img src='images/Title.png'/>
         </div>
     </div>
     )
